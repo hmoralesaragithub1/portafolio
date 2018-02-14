8a109ecdac04c0e6ff40f61393b7945b5a55aeea
Herman Morales Ara <hmoralesara@gmail.com>
2018-02-14 02:45:09 -0500

    Rutas agregadas

---
+++ src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import {AboutComponent,
+  PortafolioComponent,
+  PortfolioItemComponent
+} from "./components/index.paginas"
+
+const app_routes: Routes = [
+  { path: '', component: PortafolioComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'portfolio-item', component: PortfolioItemComponent },
+  { path: '**', pathMatch: 'full', redirectTo: '' }
+];
+
+/*useHash perimite compatibilidad con otros archivos diferentes a html5*/
+export const app_routing = RouterModule.forRoot(app_routes,{useHash:true});
