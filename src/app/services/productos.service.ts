bb143e87e2e970d370f9b0b76a3484c97ad553be
Herman Morales Ara <hmoralesara@gmail.com>
2018-02-15 00:39:38 -0500

    agregamos imagenes dinamicamente al portafolio

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -19,6 +19,7 @@ export class ProductosService {
       this.http.get('https://webpageangular.firebaseio.com/productos_idx.json')
       .subscribe(res=>{
         console.log(res.json());
+        this.productos=res.json();
         this.cargando_productos=false;
       });
     }
