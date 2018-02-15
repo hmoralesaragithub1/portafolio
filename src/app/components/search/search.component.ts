3f64daf88f540f291db6c9b0aef26d6f697779f7
Herman Morales Ara <hmoralesara@gmail.com>
2018-02-15 18:38:29 -0500

    Primera version

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,5 +1,10 @@
 import { Component} from '@angular/core';
 
+/*importamos la ruta activa para poder capturar el parametro*/
+import {ActivatedRoute} from "@angular/router";
+
+import {ProductosService} from "../../services/productos.service"
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html'
@@ -7,7 +12,18 @@ import { Component} from '@angular/core';
 })
 export class SearchComponent  {
 
-  constructor() { }
+  termino:string=undefined;
+
+  constructor(private route:ActivatedRoute,private ps:ProductosService) {
+    route.params.subscribe(parametros=>{
+    this.termino=parametros['termino'] ;
+    console.log(this.termino);
+    ps.buscar_producto(this.termino);
+
+    }
+
+    );
+   }
 
 
 }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -7,9 +7,38 @@ export class ProductosService {
 
   /*arreglo de productos*/
   productos:any[]=[];
+  productos_filtrados:any[]=[];
+
   cargando_productos:boolean=true;
   constructor(private http:Http) {
     this.cargar_productos();
+   }
+
+   public buscar_producto(termino:string){
+     // console.log("Buscando producto");
+     // console.log(this.productos.length);
+
+     if(this.productos.length===0){
+       this.cargar_productos().then(()=>{
+         /*sabemos que termino la carga gracias a la promesa*/
+         this.filtrar_productos(termino);
+       });
+     }else{
+        this.filtrar_productos(termino);
+     }
+
+
+   }
+
+   private filtrar_productos(termino:string){
+     this.productos_filtrados=[];
+     termino=termino.toLowerCase();
+     this.productos.forEach(prod=>{
+       // console.log(prod);
+       if(prod.categoria.indexOf(termino)>=0 || prod.titulo.toLowerCase().indexOf(termino)>=0){
+         this.productos_filtrados.push(prod);
+       }
+     });
    }
 
    public cargar_producto(id:string){
@@ -20,16 +49,23 @@ export class ProductosService {
 
     if(this.productos.length==0){
       this.cargando_productos=true
-      this.http.get('https://webpageangular.firebaseio.com/productos_idx.json')
-      .subscribe(res=>{
-        // console.log(res.json());
-        /*asignando un delay para ver la animacion del loader*/
-        setTimeout(()=>{
-          this.productos=res.json();
-          this.cargando_productos=false;
-        },1500);
 
-      });
+        /*agregando promesas*/
+        let promesa=new Promise((resolve,teject)=>{
+          this.http.get('https://webpageangular.firebaseio.com/productos_idx.json')
+          .subscribe(res=>{
+            // console.log(res.json());
+            /*asignando un delay para ver la animacion del loader*/
+            setTimeout(()=>{
+              this.productos=res.json();
+              this.cargando_productos=false;
+              resolve();
+            },1500);
+
+          });
+        });
+        return promesa;
+
     }
   }
 
