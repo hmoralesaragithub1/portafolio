4e9d773f621e95522d5ef76574c8069743e9d69b
Herman Morales Ara <hmoralesara@gmail.com>
2018-02-14 23:12:45 -0500

    Servicio de Firebase listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -8,15 +8,33 @@ export class InformacionService {
 
   /*creamos un objeto vacio*/
   info:any={};
+  /*creamos arreglo vacio*/
+  equipo:any[]=[];
   data_cargada:boolean=false;
+  data_cargada_sobre_nosotros:boolean=false;
 
   constructor(public http:Http) {
-      this.http.get("assets/data/info.pagina.json").
-      subscribe(data=>{
-        console.log(data.json());
-        this.info=data.json();
-        this.data_cargada=true;
-      });
+      this.carga_info();
+      this.carga_sobre_nosotros();
+   }
+
+   public carga_info(){
+     this.http.get("https://webpageangular.firebaseio.com/equipo.json").
+     subscribe(data=>{
+
+       this.equipo=data.json();
+       this.data_cargada=true;
+       console.log(this.equipo);
+     });
+   }
+
+   public carga_sobre_nosotros(){
+     this.http.get("assets/data/info.pagina.json").
+     subscribe(data=>{
+       // console.log(data.json());
+       this.info=data.json();
+       this.data_cargada_sobre_nosotros=true;
+     });
    }
 
 }
